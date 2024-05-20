45f1ab2444caac880f05976d83382396b4af7b3e
[Andrei] <[andreiaimar1362@gmail.com]>
2024-05-20 13:15:41 +0100

    Dia 20/05/2024 Boton Footer

---
+++ button.py
@@ -0,0 +1,40 @@
+from textual.app import App, ComposeResult
+from textual.containers import Horizontal, VerticalScroll, Binding
+from textual.widgets import Button, Static, Footer, Header
+
+
+class ButtonsApp(App[str]):
+    CSS_PATH = "button.tcss"
+
+    BINDINGS = [
+        Binding(key="q", action="Salir", description="Salir de la app"),
+    ]
+
+    def on_mount(self) -> None:
+        self.title = "Header Application"
+        self.sub_title = "Saludar y Salir"
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield Horizontal(
+            VerticalScroll(
+                Static("Botones Saludar", classes="header"),
+                Button("Saludar", id="Saludar"),
+            ),
+            VerticalScroll(
+                Static("Boton Salir", classes="header"),
+                Button("Salir", id="Saludar"),
+            ),
+        )
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "Saludar":
+            self.notify("Saludando a la nada")
+        else:
+            self.exit(str(event.button))
+
+
+if __name__ == "__main__":
+    app = ButtonsApp()
+    print(app.run())
